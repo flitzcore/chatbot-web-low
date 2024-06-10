d9c4139a6231d811507c613f094644f640e9e09e
lockrr <hemistergt@gmail.com>
2024-06-10 09:18:04 +0700

    change url

--- src/components/chat/chat-bottombar.tsx
+++ src/components/chat/chat-bottombar.tsx
@@ -52,7 +52,7 @@ export default function ChatBottombar({
     };
     setIsWaiting(true);
 
-    fetch("https://9af9-103-94-190-27.ngrok-free.app/predict", {
+    fetch("https://b068-103-94-190-27.ngrok-free.app/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
